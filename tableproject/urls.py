6e15b1ae70914a11593d61e7c399aba36185a6c4
rmcait <rwek5514@icloud.com>
2024-09-23 22:16:17 +0900

    urlsの変更

--- tableproject/urls.py
+++ tableproject/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from tableapp.views import indexfunc
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('tableapp.urls')),
+    path('', indexfunc, name='home'), 
 ]
